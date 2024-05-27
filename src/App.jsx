df87a95a78d07a810db19dd55580852f8012307f
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-05-27 21:51:07 +0530

    Fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,123 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-// const { v4: uuidv4 } = require('uuid');
-import { v4 as uuidv4 } from 'uuid';
-
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [showFinished, setShowFinished] = useState(true);
 
-  const [todo, setTodo] = useState("")
-  const [todos, setTodos] = useState([])
-  
-  const handleEdit = () =>{
-    
-  }
+  useEffect(() => {
+    const todoString = localStorage.getItem("todos");
+    if (todoString) {
+      const todos = JSON.parse(todoString);
+      setTodos(todos);
+    }
+  }, []);
 
-  const handleDelete = (e, id) =>{
-    let newTodos = todos.filter(item => {
-      return item.id!= id;
-    });
-    setTodos(newTodos)
-    
-  }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-  const handleAdd = () =>{
-    setTodos([...todos, {id:uuidv4(), todo, isCompleted: false}])
-    setTodo("")
-    console.log(todos)
-  }
+  const toggleFinished = () => {
+    setShowFinished(!showFinished);
+  };
 
-  const handleChange = (e) =>{
-    setTodo(e.target.value)
-  }
+  const handleEdit = (id) => {
+    const t = todos.find((i) => i.id === id);
+    setTodo(t.todo);
+    const newTodos = todos.filter((item) => item.id !== id);
+    setTodos(newTodos);
+  };
 
-  const handleCheckbox = (e) => {
-    let id = e.target.name;
-    let index = todos.findIndex(item => {
-      return item.id === id;
-    })
-    let newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
-    setTodos(newTodos)
-  }
-  
+  const handleDelete = (id) => {
+    const newTodos = todos.filter((item) => item.id !== id);
+    setTodos(newTodos);
+  };
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    setTodo("");
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleCheckbox = (id) => {
+    const newTodos = todos.map((item) =>
+      item.id === id ? { ...item, isCompleted: !item.isCompleted } : item
+    );
+    setTodos(newTodos);
+  };
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className="container mx-auto bg-violet-50 my-5 rounded-xl p-5 min-h-[80vh]">
-          <div className="addTodo my-5">
-            <h2 className='text-lg font-bold'>Add a todo</h2>
-            <input onChange={handleChange} value={todo} type="text" className='w-80' />
-            <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-6'>Save</button>
-          </div>
-          <h2 className='text-lg font-bold'>Your daily tasks</h2>
-          <div className="todos">
-            {todos.length === 0 && <div className='m-5'>No Tasks Pending</div> }
-            {todos.map(item=>{           
-            return <div key={item.id} className="todo flex justify-between w-1/4 my-3">
-             <div className='flex gap-5'>
-              <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted}  id="" />
-              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
-             </div>           
-             <div className="buttons">
-              <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1'>Edit</button>
-              <button onClick={(e)=>{handleDelete(e, item.id)}} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1'>Delete</button>
-             </div>
-            </div>
-            })}
-          </div>
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a todo</h2>
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            className="w-80"
+          />
+          <button
+            onClick={handleAdd}
+            disabled={todo.length < 3}
+            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-6 disabled:bg-red-700"
+          >
+            Save
+          </button>
+        </div>
+        <input
+          onChange={toggleFinished}
+          type="checkbox"
+          checked={showFinished}
+        />{" "}
+        Show finished
+        <h2 className="text-lg font-bold">Your daily tasks</h2>
+        <div className="todos">
+          {todos.length === 0 && <div className="m-5">No Tasks Pending</div>}
+          {todos
+            .filter((item) => showFinished || !item.isCompleted)
+            .map((item) => (
+              <div
+                key={item.id}
+                className="todo flex justify-between w-1/4 my-3"
+              >
+                <div className="flex gap-5">
+                  <input
+                    name={item.id}
+                    onChange={() => handleCheckbox(item.id)}
+                    type="checkbox"
+                    checked={item.isCompleted}
+                  />
+                  <div className={item.isCompleted ? "line-through" : ""}>
+                    {item.todo}
+                  </div>
+                </div>
+                <div className="buttons flex">
+                  <button
+                    onClick={() => handleEdit(item.id)}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(item.id)}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
