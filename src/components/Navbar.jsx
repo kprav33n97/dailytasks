3c499f25aee5639ae6e38fd86335c9b45b6d1c34
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-06-19 21:52:18 +0530

    favicon update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between bg-slate-900 text-white'>
         <div className="logo mx-9 my-2">
-            <span className='font-bold text-xl'>dailyTasks!</span>
+            {/* <span className='font-bold text-xl'>dailyTasks!</span> */}
+            <img src={logo} height={60} alt="Sit logo" />
         </div>
       <ul className='flex gap-8 mx-9 my-2'>
        <li className='cursor-pointer transition-all hover:font-bold'>Home</li>
